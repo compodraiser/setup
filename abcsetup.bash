b089dcab3d5f5328934055d0beda41995ddc023f
compodraiser <undeadnotdead44@gmail.com>
2024-02-02 14:32:00 +0300

    Update abcsetup.bash

--- abcsetup.bash
+++ abcsetup.bash
@@ -39,6 +39,7 @@ sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 sudo mkdir -p /var/www/$domain
 sudo chown -R \$USER:\$USER /var/www/$domain
 sudo rm /etc/nginx/sites-enabled/default
+rm -r /var/www/html
 sudo systemctl restart nginx
 
 # Stop Apache Service
