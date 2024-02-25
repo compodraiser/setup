46f5b49fa14195aa43d518642db8d86fe0d6ccbd
compodraiser <undeadnotdead44@gmail.com>
2024-02-25 19:05:48 +0300

    Update abcsetup.bash

--- abcsetup.bash
+++ abcsetup.bash
@@ -27,7 +27,7 @@ server {
         try_files \$uri \$uri/ =404;
     }
 
-    location ~ \.php$ {
+    location ~ \.php\$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php${PHP_VERSION}-fpm.sock;
     }
